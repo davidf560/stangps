6d69b1c501f9966c88272d21118c55280cf77dd6
Dave Flowerday <davidf@pier13.com>
2003-02-05 20:07:26 +0000

    Added support for sending of NULL character to mimic the RC

--- utils/rc_simulator.c
+++ utils/rc_simulator.c
@@ -1,65 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <termios.h>
-#include <stdio.h>
-        
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/com1"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-        
-volatile int STOP=FALSE; 
-       
-main()
+
+int main(int argc, char **argv)
 {
-  int fd,c, res;
-  struct termios options;
-  unsigned char buf[255];
-        
-  fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY ); 
-  if (fd <0) {perror(MODEMDEVICE); exit(-1); }
-        
-  tcgetattr(fd,&options); /* save current port settings */
-        
-//  cfsetispeed(&options, BAUDRATE);
-//  cfsetospeed(&options, BAUDRATE);
+  unsigned char buf[128];
+  int fd, num;
+  struct termios opts;
 
-  bzero(&options, sizeof(options));
-  options.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
-  options.c_iflag = IGNPAR;
-  options.c_oflag = 0;
-
-  // Set input mode
-  options.c_lflag = 0;
-         
-  options.c_cc[VTIME] = 0;
-  options.c_cc[VMIN] = 5;
-
-  tcflush(fd, TCIFLUSH);
-  tcsetattr(fd, TCSANOW, &options);
-        
-  while (STOP==FALSE) {
-    res = read(fd, buf, 1);
-    printf("Recvd: %d char: %02x\n", res, buf[0]);
-    if(buf[0] == 'X') {
-      STOP = TRUE;
-    }
+  if((fd = open("/dev/com1", O_RDWR | O_NOCTTY)) == -1) {
+    printf("Unable to open port.\n");
+    exit(1);
   }
 
-  STOP = FALSE;
-  read(fd, buf, 4);
-        
-  while (STOP==FALSE) {       /* loop for input */
-    res = read(fd,buf,5);   /* returns after 5 chars have been input */
-    buf[res]=0;               /* so we can printf... */
-    printf("%c: %d.%02d ", buf[0], (buf[2] * 256) + buf[3], 
-        (buf[4] * 100) / 256);
-    if(buf[0] == 'X') {
-      printf("\n");
-    }
-    if (buf[0]=='z') STOP=TRUE;
+  tcgetattr(fd, &opts);
+  opts.c_lflag &= ~(ICANON | ECHO | ISIG);
+  opts.c_cflag |= (CLOCAL | CREAD);
+  cfsetispeed(&opts, B38400);
+  cfsetospeed(&opts, B38400);
+  opts.c_cflag &= ~CRTSCTS;
+  opts.c_iflag |= (INPCK | ISTRIP);
+  opts.c_iflag &= ~(IXON | IXOFF | IXANY);
+  opts.c_cflag &= ~CSIZE;
+  opts.c_cflag |= CS8;
+  opts.c_oflag &= ~OPOST;
+  opts.c_cc[VMIN] = 1;
+  opts.c_cc[VTIME] = 5;
+  tcsetattr(fd, TCSANOW, &opts);
+
+  while(1) {
+    write(fd, "\0", 1);
+
+    while((num = read(fd, buf, 1)) < 1);
+    printf("X: %d ", buf[0]);
+
+    while((num = read(fd, buf, 1)) < 1);
+    printf("Y: %d ", buf[0]);
+
+    while((num = read(fd, buf, 1)) < 1);
+    printf("Theta: %d\n", buf[0]);
+   
   }
+
+  close(fd);
 }
-    
