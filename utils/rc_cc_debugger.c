5b209ac6be6b234316b33c6d37bb1a3e8223905b
Dave Flowerday <davidf@pier13.com>
2003-02-08 17:38:22 +0000

    Initial version

---
+++ utils/rc_cc_debugger.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+
+#define  ATTN_CHAR 0x06
+
+int main(int argc, char **argv)
+{
+  unsigned char buf[128];
+  int fd, num;
+  struct termios opts;
+
+  if((fd = open("/dev/com1", O_RDWR | O_NOCTTY)) == -1) {
+    printf("Unable to open port.\n");
+    exit(1);
+  }
+
+  tcgetattr(fd, &opts);
+  opts.c_lflag &= ~(ICANON | ECHO | ISIG);
+  opts.c_cflag |= (CLOCAL | CREAD);
+  cfsetispeed(&opts, B9600);
+  cfsetospeed(&opts, B9600);
+  opts.c_cflag &= ~CRTSCTS;
+  opts.c_iflag |= (INPCK | ISTRIP);
+  opts.c_iflag &= ~(IXON | IXOFF | IXANY);
+  opts.c_cflag &= ~CSIZE;
+  opts.c_cflag |= CS8;
+  opts.c_oflag &= ~OPOST;
+  opts.c_cc[VMIN] = 1;
+  opts.c_cc[VTIME] = 0;
+  tcsetattr(fd, TCSANOW, &opts);
+
+  while(1) {
+    write(fd, buf, 1);    
+
+    num = read(fd, buf, 1);
+    fflush(stdout);
+    if(buf[0] == ATTN_CHAR)
+    {
+      num = read(fd, buf, 3);
+      printf("X: %d ", buf[0]);
+      printf("Y: %d ", buf[1]);
+      printf("Theta: %d\n", buf[2]);
+    }
+    else
+    {
+      printf("%d", buf[0]);
+    }
+  }
+
+  close(fd);
+}
